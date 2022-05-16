37540e3d1758d01ca09a8806728e646c7b03267e
0xKubitus <n.cubilier@gmail.com>
2022-05-17 01:29:23 +0200

    emails reçus sur yopmail à la création d'un user = OK

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ class UserMailer < ApplicationMailer
     @user = user 
 
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url  = 'https://eventbrite-nico.herokuapp.com/' # <= l'adresse de mon site
+    @url  = 'https://eventbrite-nico.herokuapp.com/' # <= page d'accueil mon site, à modifier
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @user.email, subject: 'Bienvenue chez The Events Project by Kubitus ! (compte utilisateur créé)') 
